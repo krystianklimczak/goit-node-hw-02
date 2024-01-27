671aefec303fcbfa909081ec181c8f1cfa8203a9
krystianklimczak <krystianklimczak1996r.com>
2024-01-27 16:18:47 +0100

    implement authorization for all contacts

--- controllers/contacts/showContacts.js
+++ controllers/contacts/showContacts.js
@@ -1,14 +1,24 @@
 import { getContactById } from '#service/index.js';
 
 async function showContacts(req, res, next) {
+  const [user] = res.user;
   const { contactId } = req.params;
+
   try {
-    const result = await getContactById(contactId);
+    const result = await getContactById(contactId, user._id);
+
     if (result) {
-      return res.json({
+      return res.status(200).json({
         status: 'success',
         code: 200,
         data: { contact: result },
+      });
+    } else {
+      return res.status(404).json({
+        status: 'fail',
+        code: 404,
+        data: 'none',
+        message: 'Contact not found',
       });
     }
   } catch (error) {
