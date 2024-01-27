8365ce80e5aedd0d9315c209d20d230042ef8127
krystianklimczak <krystianklimczak1996r.com>
2024-01-27 16:17:11 +0100

    add use case for users

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import logger from 'morgan';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import { router as contactsRouter } from './routes/api/contacts.js';
+
+import usersRouter from './routes/api/users.js';
+import setJWTStrategy from './config/jwt.js';
+import authMiddleware from './middlewares/jwt.js';
+
+dotenv.config();
 
 const app = express();
 
@@ -12,7 +19,11 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/contacts', contactsRouter);
+setJWTStrategy();
+
+app.use('/api/users', usersRouter);
+
+app.use('/api/contacts', authMiddleware, contactsRouter);
 
 app.use((req, res) => {
   return res.status(404).json({ message: 'Not found' });
