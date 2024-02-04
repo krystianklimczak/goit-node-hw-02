230232f35a13a88881de39e96034f7b4b6daf661
krystianklimczak <krystianklimczak1996r.com>
2024-02-04 18:41:58 +0100

    fetching with master with small changes

--- service/schemas/contact.js
+++ service/schemas/contact.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
+import getAvatar from '../../handlers/getAvatar.js';
 
 const contacts = new Schema(
   {
@@ -16,6 +17,9 @@ const contacts = new Schema(
     favorite: {
       type: Boolean,
       default: false,
+    },
+    avatarURL: {
+      type: String,
     },
   },
   { versionKey: false, timestamps: true }
