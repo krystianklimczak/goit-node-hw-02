4093421c67e7e5bf3c73e328ad166fa1095c2ad3
krystianklimczak <krystianklimczak1996r.com>
2024-02-08 21:36:23 +0100

    implement new routes for verify users, send and resend emails

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,18 +1,22 @@
 import express from 'express';
 
 import authMiddleware from '#middlewares/jwt.js';
-import { signUp } from '#controllers/users/signupUser.js';
+import { verify } from '#controllers/users/verify.js';
+import { upload } from '#multerConfig/multerConfig.js';
 import { logIn } from '#controllers/users/loginUser.js';
+import { signUp } from '#controllers/users/signupUser.js';
 import { logOut } from '#controllers/users/logoutUser.js';
 import { currentUser } from '#controllers/users/currentUser.js';
+import { resendVerifyEmail } from '#controllers/users/resendVerify.js';
 import { updateUserAvatar } from '#controllers/users/updateUserAvatar.js';
-import { upload } from '../../multerConfig/multerConfig.js';
 
 const router = express.Router();
 
-router.post('/signup', signUp);
 router.post('/login', logIn);
+router.post('/signup', signUp);
+router.post('/verify', resendVerifyEmail);
 router.get('/logout', authMiddleware, logOut);
+router.get('/verify/:verificationToken', verify);
 router.get('/current', authMiddleware, currentUser);
 router.patch('/avatars', authMiddleware, upload.single('avatar'), updateUserAvatar);
 
