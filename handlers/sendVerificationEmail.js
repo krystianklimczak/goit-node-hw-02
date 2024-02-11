ce1450492bfaa52ab63979f134c156237afceb33
krystianklimczak <krystianklimczak1996r.com>
2024-02-11 09:28:08 +0100

    change email message

--- handlers/sendVerificationEmail.js
+++ handlers/sendVerificationEmail.js
@@ -13,10 +13,10 @@ const sendVerificationEmail = async (email, verificationToken) => {
     text: 'Copy and paste link on your browser or press link to verify your account',
     html: ` <div style="max-width: 600px; margin: 0 auto; padding: 20px;">
                 <h2 style="color: #333333;">Witaj!</h2>
-                <p style="color: #666666;">Dziękujemy za założenie konta. Aby potwierdzić swoje konto, kliknij poniższy link:</p>
+                <p style="color: #666666;">Thanks for creating account. To verify your account click in link below or copy paste in browser:</p>
                 <p><a href="http://localhost:3000/api/users/verify/${verificationToken}" style="display: inline-block; padding: 10px 20px; background-color: #007bff; color: #ffffff; text-decoration: none;">http://localhost:3000/api/users/verify/${verificationToken}</a></p>
-                <p style="color: #666666;">Jeśli nie zakładałeś konta, zignoruj tę wiadomość.</p>
-                <p style="color: #666666;">Dziękujemy,<br>Zespół Twojej Aplikacji</p>
+                <p style="color: #666666;">If you were not creating account, please ignore this message.</p>
+                <p style="color: #666666;">Dziękujemy,<br>Team of Crypto</p>
             </div>`,
   };
 
